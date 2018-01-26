9febd47d42756c4ba7077d99aa06abda95e017f2
Billy Wong <bwong5@stuy.edu>
2018-01-26 14:45:15 -0500

    first completed version

---
+++ lambda_function.py
@@ -0,0 +1,152 @@
+from vc_check import *
+import time, datetime
+
+app_id = 'amzn1.ask.skill.9c45de62-65e3-41f2-bd27-8486d1cb1949'
+
+def lambda_handler(event, context):
+    if event['session']['application']['applicationId'] != app_id:
+        raise ValueError('Invalid Application ID')
+    
+    if event['session']['new']:
+        on_session_started({'requestId':event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == 'LaunchRequest':
+        return on_launch(event['request'], event['session'])
+    if event['request']['type'] == 'IntentRequest':
+        return handle_intent(event['request'], event['session'])
+    if event['request']['type'] == 'SessionEndedRequest':
+        return on_session_ended(event['request'], event['session'])
+
+
+#-------------Start and Launch-----------------
+    
+def on_session_started(request_id, session):
+    pass
+    
+def on_launch(launch_request, session):
+    return get_welcome_response()
+
+def get_welcome_response():
+    title = "Welcome"
+    text = "Welcome to VC Checker. " \
+           "You can ask for the price of a virtual currency. " \
+           "Say \"Tell me the price of\"" \
+           " currency to get the price"
+    reprompt_text = "Say \"Tell me the price of\"" \
+                    " currency to get the price"
+    should_end_session = False
+    statement = build_statement(title,text,reprompt_text, should_end_session)
+    return build_response(statement,{})
+
+#------------Intent Requests--------------------
+
+def handle_intent(intent_request, session):
+    intent = intent_request['intent']
+
+    if intent['name'] == 'GetPrice':
+        return get_price(intent, session)
+    elif intent['name'] == 'AMAZON.HelpIntent':
+        return help_intent()
+    elif intent['name'] == 'AMAZON.CancelIntent':
+        return cancel_intent()
+    elif intent['name'] == 'AMAZON.StopIntent':
+        return stop_intent()
+    else:
+        raise ValueError('Invalid intent: %s' % (intent['name']))
+
+#---------Custom Intent Requests-------------
+
+def get_price(intent, session):
+
+    title = intent['name']
+    text = 'Error occurred. Please try again'
+    should_end_session = False
+
+    if 'Currency' in intent['slots']:
+        speech = intent['slots']['Currency']['value']
+        currency = speech_to_currency(speech.lower())
+        if currency != None:
+            res = call_api(currency)
+            if res != None:
+                #text = 'The price of 1 %s as of %s is $%s' % (speech, get_date_from_epoch(res['last_updated']), res['price_usd'])
+                text = 'The price of 1 %s is $%s' % (speech, res['price_usd'])
+        else:
+            text = 'That virtual currency is not supported by this skill. Sorry!'    
+
+    statement = build_statement(title,text,None,should_end_session)
+    return build_response(statement,{})
+
+
+def get_date_from_epoch(seconds):
+    seconds = int(seconds)
+    num_to_month = {
+        1: 'January',
+        2: 'February',
+        3: 'March',
+        4: 'April',
+        5: 'May',
+        6: 'June',
+        7: 'July',
+        8: 'August',
+        9: 'September',
+        10: 'October',
+        11: 'November',
+        12: 'December'
+    }
+    date = datetime.datetime.fromtimestamp(seconds)
+    return '%s %d, %d %s' % (num_to_month[date.month], date.day, date.year, time.strftime('%I:%M:%S%p', time.localtime(seconds)))
+
+#--------Required Intent Requests---------------
+
+def help_intent():
+    return get_welcome_response()
+    
+def cancel_intent():
+    return session_end_request()
+
+def stop_intent():
+    return session_end_request()
+
+def session_end_request():
+    card_title = "Session Ended"
+    speech_output = "Thank you for using the VC Checker skill. " \
+                    "Have a nice day! "
+    # Setting this to true ends the session and exits the skill.
+    should_end_session = True
+    return build_response(build_statement(
+        card_title, speech_output, None, should_end_session),{})
+
+
+def on_session_ended(request,session):
+    pass
+
+
+#-----------------Builders-----------------
+
+def build_statement(title, text, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': text
+        },
+        'card': {
+            'type': "Simple",
+            'title': title,
+            'content': text
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+def build_response(statement, session_attributes):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': statement
+    }
---
+++ vc_check.py
@@ -0,0 +1,75 @@
+import urllib, json, requests
+
+URL_HEAD = 'https://api.coinmarketcap.com/v1/ticker/'
+#other source is https://www.cryptocompare.com
+#https://coinmarketcap.com/api/
+
+#to standardize make the currency lower case
+#replace spaces with dashes
+
+
+def speech_to_currency(speech):
+    speech_currency = {
+        'bitcoin': 'bitcoin',
+        'ethereum': 'ethereum',
+        'ripple': 'ripple',
+        'bitcoin cash': 'bitcoin-cash',
+        'cardando': 'cardano',
+        'stellar': 'stellar',
+        'litecoin': 'litecoin',
+        'neo': 'neo',
+        'nem': 'nem',
+        'iota': 'iota',
+        'dash': 'dash',
+        'monero': 'monero',
+        'bitcoin gold': 'bitcoin-gold',
+        'v chain': 'vechain',
+        'qtum': 'qtum',
+        'ethereum classic': 'ethereum-classic',
+        'lisk': 'lisk',
+        'rai Blocks': 'raiblocks',
+        'steem': 'steem',
+        'zcash': 'zcash',
+        'bytecoin': 'bytecoin'
+    }
+    if speech in speech_currency:
+        return speech_currency[speech]
+    return None
+    
+def call_api(currency):
+    url = URL_HEAD + currency
+    try:
+        r = requests.get(url)
+        return r.json()[0]
+    except:
+        print url
+        print 'Error occurred when retrieving data'
+        return None
+
+
+
+
+
+'''
+Sample response
+
+[
+    {
+        "id": "bitcoin", 
+        "name": "Bitcoin", 
+        "symbol": "BTC", 
+        "rank": "1", 
+        "price_usd": "11129.0", 
+        "price_btc": "1.0", 
+        "24h_volume_usd": "8170750000.0", 
+        "market_cap_usd": "187267816548", 
+        "available_supply": "16827012.0", 
+        "total_supply": "16827012.0", 
+        "max_supply": "21000000.0", 
+        "percent_change_1h": "-2.65", 
+        "percent_change_24h": "-3.06", 
+        "percent_change_7d": "-4.43", 
+        "last_updated": "1516954166"
+    }
+]
+'''
